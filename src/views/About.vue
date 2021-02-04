47633895d6cd5a65c569692e09d964a1d85f5b4a
steve zhang <871506263@qq.com>
2021-02-04 15:09:19 +0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <keyboard></keyboard>
+  </div>
+</template>
+
+<script>
+import keyboard from 'keyboard-comp-zjh';
+export default {
+  components: {
+    keyboard
+  }
+};
+</script>
+
+<style lang="less" scoped></style>
